a547baf1c73534eef199c8eaef348fc26704cbad
ViniciusCorreia <48775082+ViniciusCorreia@users.noreply.github.com>
2019-05-17 00:04:42 -0300

    Add files via upload

--- indice.py
+++ indice.py
@@ -5,26 +5,26 @@ leitura2 = open("indice.txt","w")
 
 
 colunaNIS = 7 
-l = leitura.readline()
+
 
 
 def escolheOnis(l):
     return l[0]
 
 linhas = []
-i=1
-
-for linha in leitura:
+leitura.seek(0,2)
+tamanho = leitura.tell()
+leitura.seek(0)
+leitura.readline()
+while (leitura.tell() < tamanho):
     
+    i = leitura.tell()
     l = leitura.readline()
     linha_fragmentada = l.split("\t")
     nis = linha_fragmentada[7]
     
     linhas.append((nis,i))
     
-    i = i + 1
-
-
 
 linhas = sorted(linhas, key = escolheOnis)
 tamanholista = len(linhas)
@@ -37,7 +37,7 @@ for j in range(0,tamanholista):
     
     leitura2.write("O nis ")
     leitura2.write(li[0])
-    leitura2.write(" está na linha:")
+    leitura2.write(" está na posicao:")
     leitura2.write(str(li[1]))
     leitura2.write("\n")
 
